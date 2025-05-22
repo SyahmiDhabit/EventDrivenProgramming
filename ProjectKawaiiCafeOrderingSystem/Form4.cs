e0faf7b6f710528778a167d1887257431db93117
Chew0626 <chewweilun26@gmail.com>
2025-05-22 20:42:14 +0800

    fix form 4

--- ProjectKawaiiCafeOrderingSystem/Form4.cs
+++ ProjectKawaiiCafeOrderingSystem/Form4.cs
@@ -16,5 +16,10 @@ namespace ProjectKawaiiCafeOrderingSystem
         {
             InitializeComponent();
         }
+
+        private void receiptForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
