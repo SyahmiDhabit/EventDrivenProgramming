9d7c6f440087a031248bc707af3bfd26762c069c
SyahmiDhabit <d032310235@student.utem.edu.my>
2025-06-17 00:44:20 +0800

    updated menu data

---
+++ ProjectKawaiiCafeOrderingSystem/dbo.Menu.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Menu] (
+    [menu_ID]    INT            NOT NULL,
+    [menu_name]  VARCHAR (100)   NOT NULL,
+    [menu_price] DECIMAL (10, 2) NOT NULL,
+    PRIMARY KEY CLUSTERED ([menu_ID] ASC)
+);
+
