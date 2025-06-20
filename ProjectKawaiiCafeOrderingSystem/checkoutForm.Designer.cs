aba12b8f01ba0284e20958587f4765966042886c
Aziz3242 <abdulaziz412005@gmail.com>
2025-06-20 20:51:35 +0800

    dasdas

--- ProjectKawaiiCafeOrderingSystem/checkoutForm.Designer.cs
+++ ProjectKawaiiCafeOrderingSystem/checkoutForm.Designer.cs
@@ -19,7 +19,7 @@
         {
             this.labelcheckout = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.labelSummary = new System.Windows.Forms.Label(); // ADD THIS
+            this.labelSummary = new System.Windows.Forms.Label();
             this.labelcardnum = new System.Windows.Forms.Label();
             this.textBoxCardNum = new System.Windows.Forms.TextBox();
             this.labelCVV = new System.Windows.Forms.Label();
@@ -30,86 +30,115 @@
             this.radioDebit = new System.Windows.Forms.RadioButton();
             this.buttonCheckout = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
+            // 
             // labelcheckout
+            // 
             this.labelcheckout.AutoSize = true;
             this.labelcheckout.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
             this.labelcheckout.Location = new System.Drawing.Point(30, 20);
             this.labelcheckout.Name = "labelcheckout";
             this.labelcheckout.Size = new System.Drawing.Size(200, 31);
+            this.labelcheckout.TabIndex = 0;
             this.labelcheckout.Text = "Checkout Page";
-
+            // 
             // listBox1
+            // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(30, 70);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(400, 180);
-
-            // labelSummary (ADDED)
+            this.listBox1.TabIndex = 1;
+            // 
+            // labelSummary
+            // 
             this.labelSummary.AutoSize = true;
             this.labelSummary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.labelSummary.Location = new System.Drawing.Point(30, 270);
             this.labelSummary.Name = "labelSummary";
             this.labelSummary.Size = new System.Drawing.Size(0, 20);
-            this.labelSummary.Text = "";
-
-            // radioCash
-            this.radioCash.AutoSize = true;
-            this.radioCash.Location = new System.Drawing.Point(30, 310);
-            this.radioCash.Name = "radioCash";
-            this.radioCash.Text = "Cash";
-
-            // radioDebit
-            this.radioDebit.AutoSize = true;
-            this.radioDebit.Location = new System.Drawing.Point(100, 310);
-            this.radioDebit.Name = "radioDebit";
-            this.radioDebit.Text = "Debit";
-
+            this.labelSummary.TabIndex = 2;
+            // 
             // labelcardnum
+            // 
             this.labelcardnum.AutoSize = true;
             this.labelcardnum.Location = new System.Drawing.Point(30, 350);
             this.labelcardnum.Name = "labelcardnum";
+            this.labelcardnum.Size = new System.Drawing.Size(90, 16);
+            this.labelcardnum.TabIndex = 5;
             this.labelcardnum.Text = "Card Number:";
-
+            // 
             // textBoxCardNum
+            // 
             this.textBoxCardNum.Location = new System.Drawing.Point(150, 350);
             this.textBoxCardNum.Name = "textBoxCardNum";
             this.textBoxCardNum.Size = new System.Drawing.Size(200, 22);
-
+            this.textBoxCardNum.TabIndex = 6;
+            // 
             // labelCVV
+            // 
             this.labelCVV.AutoSize = true;
             this.labelCVV.Location = new System.Drawing.Point(30, 380);
             this.labelCVV.Name = "labelCVV";
+            this.labelCVV.Size = new System.Drawing.Size(37, 16);
+            this.labelCVV.TabIndex = 7;
             this.labelCVV.Text = "CVV:";
-
+            // 
             // textBoxCVV
+            // 
             this.textBoxCVV.Location = new System.Drawing.Point(150, 380);
             this.textBoxCVV.Name = "textBoxCVV";
             this.textBoxCVV.Size = new System.Drawing.Size(60, 22);
-
+            this.textBoxCVV.TabIndex = 8;
+            // 
             // labelAmount
+            // 
             this.labelAmount.AutoSize = true;
             this.labelAmount.Location = new System.Drawing.Point(30, 410);
             this.labelAmount.Name = "labelAmount";
+            this.labelAmount.Size = new System.Drawing.Size(55, 16);
+            this.labelAmount.TabIndex = 9;
             this.labelAmount.Text = "Amount:";
-
+            // 
             // textBoxAmount
+            // 
             this.textBoxAmount.Location = new System.Drawing.Point(150, 410);
             this.textBoxAmount.Name = "textBoxAmount";
             this.textBoxAmount.Size = new System.Drawing.Size(100, 22);
-
+            this.textBoxAmount.TabIndex = 10;
+            // 
+            // radioCash
+            // 
+            this.radioCash.AutoSize = true;
+            this.radioCash.Location = new System.Drawing.Point(30, 310);
+            this.radioCash.Name = "radioCash";
+            this.radioCash.Size = new System.Drawing.Size(59, 20);
+            this.radioCash.TabIndex = 3;
+            this.radioCash.Text = "Cash";
+            // 
+            // radioDebit
+            // 
+            this.radioDebit.AutoSize = true;
+            this.radioDebit.Location = new System.Drawing.Point(100, 310);
+            this.radioDebit.Name = "radioDebit";
+            this.radioDebit.Size = new System.Drawing.Size(60, 20);
+            this.radioDebit.TabIndex = 4;
+            this.radioDebit.Text = "Debit";
+            // 
             // buttonCheckout
+            // 
             this.buttonCheckout.Location = new System.Drawing.Point(300, 450);
             this.buttonCheckout.Name = "buttonCheckout";
-            this.buttonCheckout.Text = "Pay Now";
             this.buttonCheckout.Size = new System.Drawing.Size(100, 40);
-
+            this.buttonCheckout.TabIndex = 11;
+            this.buttonCheckout.Text = "Pay Now";
+            // 
             // checkoutForm
-            this.ClientSize = new System.Drawing.Size(500, 520);
+            // 
+            this.ClientSize = new System.Drawing.Size(832, 520);
             this.Controls.Add(this.labelcheckout);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.labelSummary); // ADD THIS
+            this.Controls.Add(this.labelSummary);
             this.Controls.Add(this.radioCash);
             this.Controls.Add(this.radioDebit);
             this.Controls.Add(this.labelcardnum);
@@ -123,6 +152,7 @@
             this.Text = "Checkout";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
